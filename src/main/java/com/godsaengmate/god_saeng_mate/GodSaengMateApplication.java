c228a346a23fe6d8dd3c2e83620d4bae914b94c2
pogeum <qha3012@gmail.com>
2024-06-05 14:16:28 +0900

    Initial commit

---
+++ src/main/java/com/godsaengmate/god_saeng_mate/GodSaengMateApplication.java
@@ -0,0 +1,13 @@
+package com.godsaengmate.god_saeng_mate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GodSaengMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GodSaengMateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/godsaengmate/god_saeng_mate/GodSaengMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.godsaengmate.god_saeng_mate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GodSaengMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
